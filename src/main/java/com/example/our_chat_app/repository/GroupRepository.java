d1261e72d7ba0ba0a6bebf48cc8aab47cc95cc61
Ubaydullayevv <sardorubaydullayev70@gmail.com>
2022-04-13 13:43:25 +0500

    erfvdvsdfvdvd

--- src/main/java/com/example/our_chat_app/repository/GroupRepository.java
+++ src/main/java/com/example/our_chat_app/repository/GroupRepository.java
@@ -15,8 +15,8 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
 
     @Query(nativeQuery = true,
             value = "select count(u.id)>0 from users u\n" +
-            "join groups_users gu on u.id = gu.users_id\n" +
-            "join groups g on gu.groups_id = g.id\n" +
+            "join groups_users gu on u.id = gu.user_id\n" +
+            "join groups g on gu.group_id = g.id\n" +
             "where u.id = :from and g.id = :groupId")
     boolean checkUser(Long from, Long groupId);
     @Query(
@@ -24,10 +24,11 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
             value = "select g.id,\n" +
                     "       g.name,\n" +
                     "       (select text from group_message where group_message.group_id = g.id order by created_at desc limit 1),\n" +
-                    "       max(gm.created_at)\n" +
+                    "       max(gm.created_at) as data,\n" +
+                    "       g.group_avatar_id\n" +
                     "from groups g\n" +
-                    "join groups_users gu on g.id = gu.groups_id\n" +
-                    "join group_message gm on g.id = gm.group_id\n" +
+                    "         join groups_users gu on g.id = gu.group_id\n" +
+                    "         join group_message gm on g.id = gm.group_id\n" +
                     "group by g.id, g.name"
     )
     List<Map<String, Object>> showAllGroups(Long afishaId);
