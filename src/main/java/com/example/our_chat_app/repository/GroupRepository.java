0b2f11b94f847b2c60af6dbe5d1824444df9ac8e
Ubaydullayevv <sardorubaydullayev70@gmail.com>
2022-04-13 13:03:38 +0500

    erfvdvsdfvdvd

--- src/main/java/com/example/our_chat_app/repository/GroupRepository.java
+++ src/main/java/com/example/our_chat_app/repository/GroupRepository.java
@@ -7,14 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    @Query(
-            nativeQuery = true,
-            value = "select * from groups"
-    )
-    List<GroupProjection> showAllGroups(Long afishaId);
+
 
     @Query(nativeQuery = true,
             value = "select count(u.id)>0 from users u\n" +
@@ -22,5 +19,17 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
             "join groups g on gu.groups_id = g.id\n" +
             "where u.id = :from and g.id = :groupId")
     boolean checkUser(Long from, Long groupId);
+    @Query(
+            nativeQuery = true,
+            value = "select g.id,\n" +
+                    "       g.name,\n" +
+                    "       (select text from group_message where group_message.group_id = g.id order by created_at desc limit 1),\n" +
+                    "       max(gm.created_at)\n" +
+                    "from groups g\n" +
+                    "join groups_users gu on g.id = gu.groups_id\n" +
+                    "join group_message gm on g.id = gm.group_id\n" +
+                    "group by g.id, g.name"
+    )
+    List<Map<String, Object>> showAllGroups(Long afishaId);
 
 }
